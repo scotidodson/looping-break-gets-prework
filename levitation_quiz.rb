b5ec4f89495024ddef74513f4a91060f2e7b04f7
Scoti Dodson <scotidodson@gmail.com>
2018-08-15 21:15:38 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,16 @@
 
 #def levitation_quiz
 	#your code here
+#end
+
+
+def annoying
+    loop do 
+        puts "What's up, Doc?"
+        answer = gets.chomp
+        break if answer == "STOP"
+    end
+    puts "Okay, okay, jeez. I'll stop. Sorry."
 end
-
-
+ 
+annoying
